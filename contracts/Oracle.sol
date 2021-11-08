4db1931cf464f46df3eff65c7043ed0876862042
buckdy <buck3dy@gmail.com>
2021-11-08 11:57:56 -0500

    feat: oracle contract

---
+++ contracts/Oracle.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.6.6;
+
+import "./interfaces/IUniswapV2Factory.sol";
+import "./interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IOracle.sol";
+import "./lib/FixedPoint.sol";
+import "./lib/UniswapV2OracleLibrary.sol";
+import "./lib/UniswapV2Library.sol";
+
+contract Oracle is IOracle {
+    using FixedPoint for *;
+
+    uint256 public constant PERIOD = 24 hours;
+
+    IUniswapV2Pair immutable pair;
+    address public immutable token0;
+    address public immutable token1;
+    address public immutable weth;
+
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
+    uint32 public blockTimestampLast;
+    FixedPoint.uq112x112 public price0Average;
+    FixedPoint.uq112x112 public price1Average;
+
+    constructor(
+        address factory,
+        address tokenA,
+        address tokenB,
+        address weth_
+    ) public {
+        IUniswapV2Pair _pair = IUniswapV2Pair(UniswapV2Library.pairFor(factory, tokenA, tokenB));
+        pair = _pair;
+        token0 = _pair.token0();
+        token1 = _pair.token1();
+        weth = weth_;
+        price0CumulativeLast = _pair.price0CumulativeLast(); // fetch the current accumulated price value (1 / 0)
+        price1CumulativeLast = _pair.price1CumulativeLast(); // fetch the current accumulated price value (0 / 1)
+        uint112 reserve0;
+        uint112 reserve1;
+        (reserve0, reserve1, blockTimestampLast) = _pair.getReserves();
+        require(reserve0 != 0 && reserve1 != 0, "ExampleOracleSimple: NO_RESERVES");
+    }
+
+    function update() external {
+        (uint256 price0Cumulative, uint256 price1Cumulative, uint32 blockTimestamp) = UniswapV2OracleLibrary
+            .currentCumulativePrices(address(pair));
+        uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
+
+        // ensure that at least one full period has passed since the last update
+        require(timeElapsed >= PERIOD, "ExampleOracleSimple: PERIOD_NOT_ELAPSED");
+
+        // overflow is desired, casting never truncates
+        // cumulative price is in (uq112x112 price * seconds) units so we simply wrap it after division by time elapsed
+        price0Average = FixedPoint.uq112x112(uint224((price0Cumulative - price0CumulativeLast) / timeElapsed));
+        price1Average = FixedPoint.uq112x112(uint224((price1Cumulative - price1CumulativeLast) / timeElapsed));
+
+        price0CumulativeLast = price0Cumulative;
+        price1CumulativeLast = price1Cumulative;
+        blockTimestampLast = blockTimestamp;
+    }
+
+    // note this will always return 0 before update has been called successfully for the first time.
+    function getTokenAmount(uint256 amountIn) external view override returns (uint256 amountOut) {
+        if (weth == token0) {
+            amountOut = price0Average.mul(amountIn).decode144();
+        } else {
+            require(weth == token1, "ExampleOracleSimple: INVALID_TOKEN");
+            amountOut = price1Average.mul(amountIn).decode144();
+        }
+    }
+}
