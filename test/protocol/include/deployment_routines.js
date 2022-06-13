67a9d2343ceadb2a6a5ff6b4c30637b2b331b3ca
yuriarthf <yuri.fernandes@illuvium.io>
2022-06-13 16:12:58 +0700

    refactor: simplify ERC20Mock

--- test/protocol/include/deployment_routines.js
+++ test/protocol/include/deployment_routines.js
@@ -1,8 +1,23 @@
 // ACL token features and roles
 const { FEATURE_ALL } = require("../../../scripts/include/features_roles");
 
-// reimport some deployment routines from erc20/erc721 deployment packs
-const { erc20_deploy, usdt_deploy } = require("../../erc20/include/deployment_routines");
+/**
+ * Deploys ERC20 token with all the features enabled
+ *
+ * @param a0 smart contract owner, super admin
+ * @param H0 initial token holder address
+ * @returns ERC20 instance
+ */
+async function erc20_deploy(a0, H0 = a0) {
+  // deploy the token
+  const token = await erc20_deploy_restricted(a0, H0);
+
+  // enable all permissions on the token
+  await token.updateFeatures(FEATURE_ALL, { from: a0 });
+
+  // return the reference
+  return token;
+}
 
 /**
  * Deploys Escrowed Illuvium Mock used for payments in Land Sale
@@ -92,7 +107,6 @@ async function chainlink_aggregator_deploy_mock(a0) {
 // export public deployment API
 module.exports = {
   erc20_deploy,
-  usdt_deploy,
   sIlv_mock_deploy,
   illuvitars_price_oracle_deploy,
   illuvitars_price_oracle_deploy_pure,
