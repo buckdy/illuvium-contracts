dc26ae80be1b97afefb2bb91a116a81d4b5a2e18
buckdy <buck3dy@gmail.com>
2022-06-16 11:00:46 +0900

    update-deployment

--- deploy/02_deploy_AccessoryLayer.ts
+++ deploy/02_deploy_AccessoryLayer.ts
@@ -1,34 +1,10 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { deployProxy, deploy } from "../scripts/include/deployment_routines";
+import { deployProxy } from "../scripts/include/deployment_routines";
 
 module.exports = async function (hre: HardhatRuntimeEnvironment) {
   const { imxMinter } = await hre.getNamedAccounts();
 
-  const impl = await deploy(hre, "AccessoryLayer", "AccessoryLayer", []);
-
-  await deployProxy(hre, "SkinAccessoryLayer", "AccessoryLayer", ["Illuvitar Skin Accessory", "ISA", imxMinter], impl);
-  await deployProxy(hre, "BodyAccessoryLayer", "AccessoryLayer", ["Illuvitar Body Accessory", "IBA", imxMinter], impl);
-  await deployProxy(
-    hre,
-    "EyeWearAccessoryLayer",
-    "AccessoryLayer",
-    ["Illuvitar EyeWear Accessory", "IEA", imxMinter],
-    impl,
-  );
-  await deployProxy(
-    hre,
-    "HeadWearAccessoryLayer",
-    "AccessoryLayer",
-    ["Illuvitar HeadWear Accessory", "IHA", imxMinter],
-    impl,
-  );
-  await deployProxy(
-    hre,
-    "PropsAccessoryLayer",
-    "AccessoryLayer",
-    ["Illuvitar Props Accessory", "IPA", imxMinter],
-    impl,
-  );
+  await deployProxy(hre, "Illuvitar Accessory", "AccessoryLayer", ["Illuvitar Accessory", "ILA", imxMinter]);
 };
 
 module.exports.tags = ["AccessoryLayer"];
